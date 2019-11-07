2b1010b0212a60b33a63f9afe2620fe8cb9588d7
Boulay Mathias <mathiasboulay@free.fr>
2019-11-07 17:58:10 +0100

    - Added blue and filename global variables - Added Two optimize functions

--- Src/Optimizer.sh
+++ Src/Optimizer.sh
@@ -4,9 +4,12 @@
 #All functions thinks we are in the same folder as the file.
 
 GREEN = "\033[32m"
+BLUE = "\033[34m"
 WHITE = "\033[0m"
+Filename = ""
 
 for folder in ["Mods","Temp","Output"]
+do
 	if [ -d "$folder" ]
 	then
 		echo -e '$GREEN "$folder" folder found. $WHITE'
@@ -18,6 +21,49 @@ for folder in ["Mods","Temp","Output"]
 done
 
 function Unzip_mod(){
-	target = $1
-	mv 
+	target = "$1"
+	Filename = `basename "$target" `
+	mv "$target" ../Temp/"$Filename"
+	unzip -o -d ./Temp/"$Filename" ./Temp/"$Filename"
+	
 	}
+	
+function Optimize_textures(){
+	#This function assumes there is something to optimize !
+	cd Temp
+	ColorPalette = "$1"
+	for PNG in "$(find . -name '*.png' )"
+	do
+		echo -e "'$GREEN'Optimizing '$BLUE' `basename $PNG` '$WHITE'"
+		pngquant --ext .png -f --speed 1 --quiet "$ColorPalette" "$PNG"
+		
+	done
+	cd ..
+	
+	}
+	
+	
+function Optimize_audio(){
+	#This function assumes there is something to optimize !
+	cd Temp
+	Frequency = "$1"
+	Bitrate = "$2"
+	for AUDIO in $(find . -name '*.ogg'); 
+	do
+		echo -e "'$GREEN'Optimizing '$BLUE' `basename $AUDIO` '$WHITE'"
+		ffmpeg -i "$AUDIO" -v 0 -y -ar "$Frequency" -b:a "$Bitrate" -f ogg "${audio%.*}.ogg"
+	done
+	cd ..
+	
+	}
+	
+function Repackage_mod(){
+	
+	
+	
+	}
+	
+	
+	
+	
+	
